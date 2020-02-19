f7738361dd615b5198370202e415103f87e0d2dd
Nadir Alfakesh <nader.alfakesh@gmail.com>
2020-02-19 09:26:58 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,3 +8,13 @@ class Tree{
     return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves";
   }
 }
+
+
+class Tree{
+  constructor(species){
+    this.species = species;
+  }
+  static definition(){
+    return "A tree is a perennial plant with an elongated stem, or trunk, supporting branches and leaves";
+  }
+}
